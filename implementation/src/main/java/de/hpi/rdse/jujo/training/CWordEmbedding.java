fcf42e7a7b83337f97ddd09c8e4834ff47210fef
Julian Weise <privat@julianweise.de>
2019-07-26 14:20:11 +0200

    Fixed representation of cword embeddings
    Co-authored-by: Johannes-Schneider <johannes.schneider@student.hpi.de>

--- implementation/src/main/java/de/hpi/rdse/jujo/training/CWordEmbedding.java
+++ implementation/src/main/java/de/hpi/rdse/jujo/training/CWordEmbedding.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.linear.RealVectorFormat;
 
 import java.io.Serializable;
 
@@ -13,11 +14,12 @@ import java.io.Serializable;
 public class CWordEmbedding implements Serializable {
 
     private static final long serialVersionUID = 5837715591126397557L;
+    private static final RealVectorFormat cWordFormat = new RealVectorFormat("", "", " ");
     private String word;
     private RealVector weights;
 
     @Override
     public String toString() {
-        return String.format("%s %s", word, weights.toString());
+        return String.format("%s %s", word, CWordEmbedding.cWordFormat.format(weights));
     }
 }
